2a4542d7cc97a7050e075fb1de4152c429fe5c97
Gideon Sanni <gideonyte@hotmail.com>
2020-08-29 18:35:05 -0600

    Updates

---
+++ precache-manifest.1ac313f8ba817d9b4182cf77887e4991.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090cab42753b7683e3a0a51d53693c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fed5b71bedcfefec8626",
+    "url": "/static/css/main.b5648c3e.chunk.css"
+  },
+  {
+    "revision": "cbb193c1fc35c16cf0f1",
+    "url": "/static/js/2.b0bcb409.chunk.js"
+  },
+  {
+    "revision": "73c9a85f0bc7d6207b5f74d36d4a7c28",
+    "url": "/static/js/2.b0bcb409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed5b71bedcfefec8626",
+    "url": "/static/js/main.225ff1d3.chunk.js"
+  },
+  {
+    "revision": "487f93c854a99c19e224",
+    "url": "/static/js/runtime-main.0a9b5e82.js"
+  },
+  {
+    "revision": "e48976f4eb1b94ea69509bec5ded0fe2",
+    "url": "/static/media/Accounting.e48976f4.svg"
+  },
+  {
+    "revision": "afab019f9471cc8a8c2f272ef373b194",
+    "url": "/static/media/AddInformation.afab019f.svg"
+  },
+  {
+    "revision": "f0ec94e8238a859b050f29be9ac461f6",
+    "url": "/static/media/CustomerRelationsManagement.f0ec94e8.svg"
+  },
+  {
+    "revision": "43d7c3eb2b184c739ae17b44bd30f487",
+    "url": "/static/media/DatabaseImport.43d7c3eb.svg"
+  },
+  {
+    "revision": "46dca6001780c51690fc38682f945892",
+    "url": "/static/media/ExcelImport.46dca600.svg"
+  },
+  {
+    "revision": "72cde23c71be04c30cc13b4afef14f5e",
+    "url": "/static/media/FacilityManagement.72cde23c.svg"
+  },
+  {
+    "revision": "41903c91ee8f5ab102b7871816022db4",
+    "url": "/static/media/FinanceManagement.41903c91.svg"
+  },
+  {
+    "revision": "982a192aeb969b2f16bdcd01a8329f92",
+    "url": "/static/media/GraspLogo.982a192a.svg"
+  },
+  {
+    "revision": "27b5f9bfdff2b78aef6d2eb2fcc8cebb",
+    "url": "/static/media/Grasptitlelogo.27b5f9bf.svg"
+  },
+  {
+    "revision": "af79d6683a5269f89428ef4eb322775c",
+    "url": "/static/media/HomeViewBackground.af79d668.svg"
+  },
+  {
+    "revision": "aafe2abc98a2052278898e06b719dfee",
+    "url": "/static/media/InventoryManagement.aafe2abc.svg"
+  },
+  {
+    "revision": "e2fc631b7a13d63b2ac25a3eacbf3973",
+    "url": "/static/media/LogisiticsManagement.e2fc631b.svg"
+  },
+  {
+    "revision": "141cdb4a358d311573ce082951770b35",
+    "url": "/static/media/PowerpointImport.141cdb4a.svg"
+  },
+  {
+    "revision": "deaea6b3d7a5b421305bebe2c4be74cd",
+    "url": "/static/media/ProjectManagement.deaea6b3.svg"
+  }
+]);
